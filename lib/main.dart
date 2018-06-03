578cf78acba78ff9105eba85d96e52c10e680c4b
AllanJeremy <aj.dev254@gmail.com>
2018-06-03 21:49:48 +0300

    Basic rectangle app - this app displays two rectangles on the screen with varying text and customizable heights - It also has an appbar with a button

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,76 @@ class MyApp extends StatelessWidget
   Widget build(BuildContext context){
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: Text('My app name')
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('App title'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add_alert),
+                iconSize: 24.0,
+                color: Colors.white,
+                onPressed: printHello,
+              )
+            ],
           ),
           body: Container(
-            color:Colors.white10,
-            child: new Center(
-              child:Text(
-                "Hello there",
-                textDirection: TextDirection.ltr,
-                style: new TextStyle(
-                  color: Colors.black87,
-                  fontSize: 42.0
-                )
-              ),
-            )
+            margin: EdgeInsets.symmetric(vertical: 40.0),
+            child: Center(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  MyRectangle(
+                    width:300.0,
+                    height:200.0,
+                    text:'I love black',
+                    color: Colors.black87,
+                  ),
+                  MyRectangle(
+                    width:300.0,
+                    height:200.0,
+                    text:'I love blue',
+                    color: Colors.blue,
+                  ),
+                ]
+              )
+            ),
           )
         )
       );
   }
+}
+
+class MyRectangle extends StatelessWidget
+{
+  final double width,height;
+  final String text;
+  final Color color;
+
+  MyRectangle({this.width,this.height,this.text,this.color});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      margin: EdgeInsets.only(top:15.0),
+      color: this.color,
+      width: this.width,
+      height: this.height,
+      child: Center(
+        child: Text(
+          this.text,
+          style: TextStyle(
+            fontSize: 32.0,
+            color:Colors.white70
+          )
+        )
+      ),
+    );
+  }
+}
+
+//Prints hello to the screen
+void printHello()
+{
+  print("Hello");
 }
